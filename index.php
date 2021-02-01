bfa35ba13a4ebc006f77c2a040a3b47447e236a5
Quant85 <antonio.85.q@gmail.com>
2021-02-01 19:49:24 +0100

    aggiunto favicon.ico

--- index.php
+++ index.php
@@ -17,6 +17,9 @@ require è identico a include, tranne che in caso di errore produrrà anche un e
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Domande frequenti</title>
 
+  <!-- favicon -->
+  <link rel="shortcut icon" href="./assets/img/favicon.ico" type="image/x-icon">
+
   <!-- Google font -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"> 
